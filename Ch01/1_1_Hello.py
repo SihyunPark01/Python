891d8e038d48baf82d9f9c6c2a460a285d904424
maro0226 <maro0226@naver.com>
2021-08-09 16:52:55 +0900

    2021/08/09 파이썬예제

---
+++ Ch01/1_1_Hello.py
@@ -0,0 +1,14 @@
+"""
+날짜 : 2021/08/09
+이름 : 박시현
+내용 : 파이썬 개발환경 설정 교재 p15
+"""
+
+print('Hello World~') #끝에 세미콜론 안붙여도됨
+print('Hello Python!')
+
+#end 옵션을 이용한 한줄 출력
+print('Hello!', end='!') #보통은 한줄씩 출력되므로 end를 쓰면 한줄로 나옴
+print('Korea!')
+
+
